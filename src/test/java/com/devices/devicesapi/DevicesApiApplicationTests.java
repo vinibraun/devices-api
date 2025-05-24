844ca5d74f19e70b8d8499f5cc9281bc15c769b6
vinicius <braunvinicius2@gmail.com>
2025-05-24 15:26:30 -0300

    Base application and spring configuration setup

---
+++ src/test/java/com/devices/devicesapi/DevicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devices.devicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
