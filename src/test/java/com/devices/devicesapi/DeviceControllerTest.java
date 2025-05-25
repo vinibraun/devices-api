f7a942533b25959d2af432f49085e81bdf42c232
vinicius <braunvinicius2@gmail.com>
2025-05-25 19:33:25 -0300

    feat(tests): implementing new MockMvc tests for all endpoints and a setUp to pattern the device creation for the tests

--- src/test/java/com/devices/devicesapi/DeviceControllerTest.java
+++ src/test/java/com/devices/devicesapi/DeviceControllerTest.java
@@ -3,9 +3,9 @@ package com.devices.devicesapi;
 import com.devices.devicesapi.dto.DeviceDTO;
 import com.devices.devicesapi.entity.Device;
 import com.devices.devicesapi.entity.DeviceState;
-import com.devices.devicesapi.mapper.DeviceMapper;
 import com.devices.devicesapi.repository.DeviceRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -34,11 +36,26 @@ public class DeviceControllerTest { //Web Layer Integration Test
     @Autowired
     private DeviceRepository deviceRepository;
 
+    private Device testDevice;
+
+    @BeforeEach
+    void setUp() {
+        deviceRepository.deleteAll();
+
+        testDevice = new Device();
+        testDevice.setName("Smartphone X");
+        testDevice.setBrand("Samsung");
+        testDevice.setState(DeviceState.AVAILABLE);
+        testDevice.setCreatedAt(LocalDateTime.now());
+
+        testDevice = deviceRepository.save(testDevice);
+    }
+
     @Test
-    public void successDeviceCreation() throws Exception {
+    public void shouldCreateDevice() throws Exception {
         String json = """
                 {
-                    "name": "Smartphone X",
+                    "name": "Smartphone Y",
                     "brand": "Brand Y",
                     "state": "AVAILABLE"
                 }
@@ -48,19 +65,12 @@ public class DeviceControllerTest { //Web Layer Integration Test
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").value("Smartphone X"))
+                .andExpect(jsonPath("$.name").value("Smartphone Y"))
                 .andExpect(jsonPath("$.brand").value("Brand Y"));
     }
 
     @Test
     void shouldUpdateDeviceSuccessfully() throws Exception {
-        // First: create a new device in the database to be able to update
-        DeviceDTO newDevice = new DeviceDTO();
-        newDevice.setName("Smartphone A");
-        newDevice.setBrand("Brand A");
-        newDevice.setState(DeviceState.AVAILABLE);
-
-        Device savedDevice = deviceRepository.save(DeviceMapper.toEntity(newDevice));
 
         // Changes that will be sent in the body of the update
         DeviceDTO updatedDevice = new DeviceDTO();
@@ -71,13 +81,13 @@ public class DeviceControllerTest { //Web Layer Integration Test
         String jsonUpdate = objectMapper.writeValueAsString(updatedDevice);
 
         // Update request with MockMvc
-        mockMvc.perform(put("/api/devices/{id}", savedDevice.getId())
+        mockMvc.perform(put("/api/devices/{id}", testDevice.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonUpdate))
                 .andExpect(status().isOk());
 
         // Validates whether the device has actually been updated in the database
-        Device deviceAfterUpdate = deviceRepository.findById(savedDevice.getId()).orElseThrow();
+        Device deviceAfterUpdate = deviceRepository.findById(testDevice.getId()).orElseThrow();
         assertEquals("Smartphone B", deviceAfterUpdate.getName());
         assertEquals("Brand B", deviceAfterUpdate.getBrand());
         assertEquals(DeviceState.INACTIVE, deviceAfterUpdate.getState());
@@ -85,29 +95,63 @@ public class DeviceControllerTest { //Web Layer Integration Test
 
     @Test
     void shouldDeleteDeviceSuccessfully() throws Exception {
-        DeviceDTO newDevice = new DeviceDTO();
-        newDevice.setName("Device To Delete");
-        newDevice.setBrand("DeleteBrand");
-        newDevice.setState(DeviceState.AVAILABLE);
-
-        Device savedDevice = deviceRepository.save(DeviceMapper.toEntity(newDevice));
-
-        mockMvc.perform(delete("/api/devices/{id}", savedDevice.getId()))
+        mockMvc.perform(delete("/api/devices/{id}", testDevice.getId()))
                 .andExpect(status().isNoContent());
 
         // Verification of existence
-        assertFalse(deviceRepository.findById(savedDevice.getId()).isPresent());
+        assertFalse(deviceRepository.findById(testDevice.getId()).isPresent());
     }
 
     @Test
-    public void patchDeviceShouldUpdateName() throws Exception {
+    public void shouldUpdateNamePatchDevice() throws Exception {
         String jsonPatch = "{\"name\":\"newName\"}";
 
-        mockMvc.perform(patch("/api/devices/{id}", 1L)
+        mockMvc.perform(patch("/api/devices/{id}", testDevice.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonPatch))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("newName"));
+    }
+
+    @Test
+    public void shouldGetDeviceById() throws Exception {
+        mockMvc.perform(get("/api/devices/{id}", testDevice.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Smartphone X"))
+                .andExpect(jsonPath("$.brand").value("Samsung"))
+                .andExpect(jsonPath("$.state").value("AVAILABLE"));
+    }
+
+    @Test
+    public void shouldGetAllDevices() throws Exception {
+        Device secondDevice = new Device();
+        secondDevice.setName("Device 2");
+        secondDevice.setBrand("Lenovo");
+        secondDevice.setState(DeviceState.IN_USE);
+        secondDevice.setCreatedAt(LocalDateTime.now());
+        deviceRepository.save(secondDevice);
+
+        mockMvc.perform(get("/api/devices")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    void shouldReturnDevicesByBrand() throws Exception {
+        mockMvc.perform(get("/api/devices/brand/{brand}", "Samsung")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].brand").value("Samsung"));
+    }
+
+    @Test
+    void shouldReturnDevicesByState() throws Exception {
+        mockMvc.perform(get("/api/devices/state/{state}", "AVAILABLE")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].state").value("AVAILABLE"));
     }
 
 }
